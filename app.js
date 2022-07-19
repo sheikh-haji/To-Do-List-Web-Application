58b242bdace6eaae4cc679322a6eab7162091604
SHEIKH HAJI <sheikhhaji18@gmail.com>
2022-07-20 01:13:15 +0530

    lastfinal

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(session({
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://todolistcegchennai.herokuapp.com/auth/google/secrets",
      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
